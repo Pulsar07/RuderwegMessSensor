27893e09eaaeae41e403eee95ed4465e0112470f
Pulsar07 <33695040+Pulsar07@users.noreply.github.com>
2019-10-12 14:34:37 +0200

    only for individual settings, should exist

--- mySettings.h
+++
@@ -1,9 +0,0 @@
-#define MY_WIFI_SSID "0xb1c8"
-#define MY_WIFI_PASSWORD "JE3_y9zJioyT3izYSyxUSPaj"
-
-// select on of these supported sensors connceted via I2C to D1 (SCL), D2 (SDA)
-// #define SUPPORT_MMA8451
-#define SUPPORT_MPU6050
-
-// a additional local connected for OLED display
-// #define SUPPORT_OLED
