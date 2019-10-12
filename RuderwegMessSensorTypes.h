a74ec9ff37c30cc2963d24a32030645c2f74ae03
Rainer Stransky <pulsar07@so-fa.de>
2019-10-12 20:47:04 +0200

    late add to Vo.17 ;-(

--- RuderwegMessSensorTypes.h
+++ RuderwegMessSensorTypes.h
@@ -6,14 +6,34 @@
 typedef enum {
   xAxis,
   yAxis,
+  zAxis,
 } referenceAxis_t;
 
+typedef enum {
+  P001,
+  P010,
+  P050,
+  P100,
+} precision_t;
+
+#define CONFIG_VERSION "RSV1"
+#define CONFIG_VERSION_L 5
+#define CONFIG_SSID_L 16
+#define CONFIG_PASSW_L 64
 // Types 'byte' und 'word' doesn't work!
 typedef struct {
+  char version[CONFIG_VERSION_L];
   referenceAxis_t axis;
-  char SSID[31];                    // SSID of WiFi
-  char password[128];                // Password of WiFi
+  precision_t anglePrecision;
+  precision_t amplitudePrecision;
+  int amplitudeInversion;
+  int angleInversion;
+  char wlanSsid[CONFIG_SSID_L];
+  char wlanPasswd[CONFIG_PASSW_L];
+  char apPasswd[CONFIG_PASSW_L];
+  boolean apIsActive;
 } configData_t;
+
 
 
 
